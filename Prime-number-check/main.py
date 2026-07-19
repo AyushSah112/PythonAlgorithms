c80203bfb228ab305bca7817f6c529f16e4e1019
Ayush Sah <ayushsah069@gmail.com>
2026-07-19 14:59:44 +0530

    Improve input handling and prime check logic
    Refactor prime number check logic with error handling.

--- Prime-number-check/main.py
+++ Prime-number-check/main.py
@@ -7,28 +7,32 @@ def intro() :
     
     
 def body() :
-    n = input("Type a number or 'quit' :  ")
-    if n == "1" :
-        print("It is not a prime")
-        return True
-    elif n == "2" :
-        print("it is prime")
-        return True
-    elif n == "quit" :
-        return False
-    else :
-        p = int(n)
-        prime = True 
-        for i in range(2, p):
-            
-            if p%i == 0 :
-                prime = False
-        if prime:
-            print("It is prime")
+    try :
+        n = input("Type a number or 'quit' :  ")
+        if n == "1" :
+            print("It is not a prime")
             return True
-        else:
-            print("its not prime")
+        elif n == "2" :
+            print("it is prime")
             return True
+        elif n == "quit" :
+            return False
+        else :
+            p = int(n)
+            prime = True 
+            for i in range(2, p):
+                
+                if p%i == 0 :
+                    prime = False
+            if prime:
+                print("It is prime")
+                return True
+            else:
+                print("its not prime")
+                return True
+                
+    except Exception:
+        return True
     
 def main() :
     while True :
